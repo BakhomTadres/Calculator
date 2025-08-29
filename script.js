e419746063514e487d22b96bcca5a032d62a0cf3
Bakhom <bakhomytadres@gmail.com>
2025-08-29 18:21:55 +0300

    Calculator javaScript

---
+++ script.js
@@ -0,0 +1,31 @@
+let displayRead = document.querySelector("input");
+let cButton = document.querySelector(".orange");
+let equalButton = document.querySelector(".equal");
+let calculator = document.querySelector(".container");
+cButton.onclick = function () {
+  displayRead.value = "";
+};
+calculator.addEventListener("click", function (el) {
+  if (el.target.nodeName == "BUTTON" && !el.target.classList.contains("orange") && !el.target.classList.contains("equal")) {
+    displayRead.value += el.target.textContent;
+  }
+});
+calculator.addEventListener("click", function (el) {
+    if (el.target.textContent === "=") {
+        try {
+            if ( parseInt(eval(displayRead.value)) === eval(displayRead.value)) {
+                displayRead.value = eval(displayRead.value)
+            } else {
+                displayRead.value = parseFloat(eval(displayRead.value).toFixed(6))
+            }
+        } catch {
+            displayRead.value = "Math Error"
+        }
+    }
+})
+calculator.addEventListener("click", function (el) {
+    if (el.target.textContent === "DEL") {
+        let currentContent = displayRead.value;
+        displayRead.value = currentContent.substring(0, currentContent.length - 1)
+    }
+})
